b55a6da0f36af7a6f89e92ce7cf58426c275abde
alvaro alvarez <f.alvarezarg@gmail.com>
2025-08-22 23:57:53 -0300

    deshaciendo cambios

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-
-exec gunicorn ShortyShop.wsgi:application --bind 0.0.0.0:8000 --workers ${WEB_CONCURRENCY:-3}
