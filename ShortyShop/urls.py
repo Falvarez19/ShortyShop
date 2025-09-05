cc291243939aab47f8f24fb42a3a945c33980592
alvaro <f.alvarezarg@gmail.com>
2025-09-05 15:33:03 -0300

    subiendo fix de fotos

--- ShortyShop/urls.py
+++ ShortyShop/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("shop.urls")),  # Rutas de la tienda
     path("accounts/", include("accounts.urls")),  # Rutas de autenticación
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
